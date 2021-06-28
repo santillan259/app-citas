6cea8e661ad8648253cce4a762469c5a0f8132da
santillan259 <santillan259@gmail.com>
2021-06-28 02:49:35 -0500

    commit on master

---
+++ src/QuotesBox.js
@@ -0,0 +1,11 @@
+import RandomQuotes from "./components/RandomQuotes";
+
+
+
+export default function QuotesBox (){
+    return(
+        <div className="quotes-box">
+            <RandomQuotes/>
+        </div>
+    )
+};
---
+++ src/components/RandomQuotes.js
@@ -0,0 +1,11 @@
+export default function RandomQuotes () {
+    return(
+
+        <div>
+            <h2>Aqui va La cita</h2>
+            <h5>Aqui va el autor</h5>
+            <button>Compartir Twitter</button>
+            <button>Nueva Cita</button>
+        </div>
+    )
+}
